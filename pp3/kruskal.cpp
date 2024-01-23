38dea3ae601938e61d82846d8be5fd11b985e85d
CaioBertoldo <120601475+CaioBertoldo@users.noreply.github.com>
2024-01-23 17:53:31 -0400

    kruskal

--- pp3/kruskal.cpp
+++ pp3/kruskal.cpp
@@ -5,12 +5,50 @@
 
 using namespace std;
 
+class UnionFind {
+    private:
+        unsigned int *parent;
+        unsigned int num_vertices;
+    public:
+        UnionFind(unsigned int);
+        int find_set(unsigned int);
+        void make_set();
+        void Union(int, int);
+        
+};
+
+UnionFind::UnionFind(unsigned int num_vertices): num_vertices(num_vertices){
+    parent = new unsigned int[num_vertices];
+}
+
+int UnionFind::find_set(unsigned int x){
+    if(parent[x] == x){
+        return x;
+    } else{
+        return find_set(parent[x]);
+    }
+}
+
+void UnionFind::make_set(){
+    for(unsigned int i = 0; i < num_vertices; i++){
+        parent[i] = i;
+    }
+}
+
+void UnionFind::Union(int x, int y){
+    x = find_set(x);
+    y = find_set(y);
+    if(x != y){
+        parent[y] = x;
+    }
+}
+
 class Graph {
     private:
         vector<pair<pair<unsigned int,unsigned int>,float>> graph;
         vector<pair<pair<unsigned int,unsigned int>,float>> mst;
         unsigned int num_vertices;
-        unsigned int *parent;
+        // unsigned int *parent;
     public:
         Graph(unsigned int);
         ~Graph();
@@ -18,20 +56,24 @@ class Graph {
         unsigned int get_num_vertices() {return num_vertices;}
         vector<pair<pair<unsigned int,unsigned int>,float>> get_graph() {return graph;}
         vector<pair<pair<unsigned int,unsigned int>,float>> get_mst() {return mst;}
-        // Funções Union Find
-        int find_set(unsigned int);
-        void make_set(int);
-        void Union(int, int);
         void Mst_Kruskal();
-        void display_mst();
-
         // Bubble sort
         void bubble_sort(vector<pair<pair<unsigned int, unsigned int>, float>>);
+        void display_mst();
+
 };
+
+
+
+void Graph::display_mst(){
+    for(unsigned int i = 0; i < mst.size(); i++){
+        cout << mst[i].first.first << " "  <<  mst[i].first.second << " " << mst[i].second << endl;
+    }
+}
 
 Graph::Graph(unsigned int num_vertices){
     this->num_vertices = num_vertices;
-    parent = new unsigned int[num_vertices];
+    // parent = new unsigned int[num_vertices];
 }
 
 Graph::~Graph(){
@@ -49,28 +91,8 @@ void Graph::add_edge(unsigned int u, unsigned int v, float w){
     graph.push_back(edge);
 }
 
-int Graph::find_set(unsigned int x){
-    if(parent[x] == x){
-        return x;
-    } else{
-        return find_set(parent[x]);
-    }
-}
-
-void Graph::make_set(int x){
-    parent[x] = x;
-}
-
-void Graph::Union(int x, int y){
-    x = find_set(x);
-    y = find_set(y);
-    if(x != y){
-        parent[y] = x;
-    }
-}
-
 void Graph::bubble_sort(vector<pair<pair<unsigned int, unsigned int>, float>> g){
-    //pair<pair<unsigned int, unsigned int>, float> aux;
+    pair<pair<unsigned int, unsigned int>, float> aux;
     for(unsigned int i = 0; i < g.size(); i++){
         for(unsigned int j = 0; j < g.size() - i - 1; j++){
             if(g[j].second > g[j + 1].second){
@@ -81,25 +103,21 @@ void Graph::bubble_sort(vector<pair<pair<unsigned int, unsigned int>, float>> g)
 }
 
 void Graph::Mst_Kruskal(){
-    for(unsigned int i = 0; i < num_vertices; i++){
-        make_set(i);
-    }
+    UnionFind u{num_vertices};
+    u.make_set();
     bubble_sort(graph);
 
     for(unsigned int i = 0; i < graph.size(); i++){
-        if(find_set(graph[i].first.first) != find_set(graph[i].first.second)){
+        if(u.find_set(graph[i].first.first) != u.find_set(graph[i].first.second)){
             mst.push_back(graph[i]);
-            Union(graph[i].first.first, graph[i].first.second);
+            u.Union(graph[i].first.first, graph[i].first.second);
+            //cout<< "teste " << mst[i].first.first<<endl;
         }
     }
-    //cout << mst.size();
+    //cout <<"size: "<< mst.size()<<endl;
 }
 
-void Graph::display_mst(){
-    for(unsigned int i = 0; i < mst.size(); i++){
-        cout << mst[i].first.first << " "  <<  mst[i].first.second << " " << mst[i].second << endl;
-    }
-}
+
 
 int main(){
 
